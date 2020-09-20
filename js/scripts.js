b935f1969df9d4b0c4a7e68be21bad5ec7b2dfce
Saharsh Sodani <EN18CS301216@medicaps.ac.in>
2020-09-20 21:55:03 +0530

    Updated safety measures

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
 (function($) {
-    AOS.init();
+
+  $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    
+
+    new WOW().init();
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('.navbar-collapse ul li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggle:visible').click();
+    });
+
+  AOS.init();
 })(jQuery)
